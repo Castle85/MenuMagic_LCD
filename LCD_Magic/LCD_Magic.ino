9fc9afa1975987989ac141fd7dea1620a6a40e4e
Joel Castillo Martinez <joel_castillo_martinez@hotmail.com>
2018-02-09 15:48:46 +0100

    Agregar archivos de proyecto.

---
+++ LCD_Magic/LCD_Magic.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LCD_Magic.ino
+ Created:	09/02/2018 15:48:32
+ Author:	joel_
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
